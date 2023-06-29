ba4585249b88596965ac962115a28509098adc01
a_shalamov <a.shalamov@s7.ru>
2023-06-29 18:32:48 +0300

    L6: add ruby and monga install

---
+++ install_mongodb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Mongo install..."
+
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc |  gpg --dearmor | sudo tee /usr/share/keyrings/mongodb.gpg > /dev/null
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+sudo systemctl status mongod
