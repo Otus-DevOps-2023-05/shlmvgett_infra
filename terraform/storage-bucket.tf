70c218a628226745e8e7db2eb5e6ef8383ef25dc
usr@email.com <a.shalamov@s7.ru>
2023-07-26 20:42:12 +0300

    Fix vars

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -4,10 +4,3 @@ provider "yandex" {
   folder_id = var.folder_id
   zone      = var.zone
 }
-
-resource "yandex_storage_bucket" "reddit-bucket" {
-  bucket        = var.bucket_name
-  access_key    = var.access_key
-  secret_key    = var.secret_key
-  force_destroy = "true"
-}
